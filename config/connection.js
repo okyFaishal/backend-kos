d8c6617e688fbe81798375121cd1fcb610cedee3
okyFaishal <okyfaisal.0@gmail.com>
2022-10-10 20:51:03 +0700

    db

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,15 @@
 require('dotenv').config()
 const { Sequelize } = require('sequelize');
-
+// console.log(process.env)
 const sequelize = new Sequelize(process.env.DB_NAME,  process.env.DB_USERNAME,  process.env.DB_PASSWORD, {
   host:  process.env.DB_HOST,
-  port:  process.env.PORT,
+  port:  process.env.DB_PORT,
+
+// const sequelize = new Sequelize('kos_app',  'postgres',  'Grafika9', {
+//   host:  'serova.id',
+//   port:  8000,
   dialect: 'postgres',
+  logging: false,
   define: {
     freezeTableName: true,
     underscored: true,
